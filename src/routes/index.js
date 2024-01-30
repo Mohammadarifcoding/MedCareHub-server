b8cdeae56b911979667ee4daf954590e56fce776
iamapusaha <apusaha392@gmail.com>
2024-01-30 12:01:51 +0600

    added post api for forum data

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,20 @@
 const { exampleDataApi, BestDoctors, BestMedicine, } = require('../api')
+const { savedFrormPost } = require('../api/forum')
+
 
 
 const router = require('express').Router()
 
-router.get('/data/:id',exampleDataApi)
+router.get('/data/:id', exampleDataApi)
 
 
 
-router.get('/Doctors',BestDoctors)
+router.get('/Doctors', BestDoctors)
 
 
-router.get('/Medicines',BestMedicine)
+router.get('/Medicines', BestMedicine)
+
+router.post('/forum', savedFrormPost)
 
 
 
