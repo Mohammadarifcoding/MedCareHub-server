7645f5364594b36bb69e4938f9dc558303d3871a
Mohammadarifcoding <minaraarifa@gmail.com>
2024-02-06 20:13:41 +0600

    Solved some issue with code

--- src/api/forum.js
+++ src/api/forum.js
@@ -1,23 +1,31 @@
-const { insertForumData, getForumDataFromCollection, getForumDatabymail, addedCommnetById } = require("../lib/forum");
-
+const {
+  insertForumData,
+  getForumDataFromCollection,
+  getForumDatabymail,
+  addedCommnetById,
+} = require("../lib/forum");
 
 const savedFrormPost = async (req, res) => {
-    const postData = req.body;
-    const result = await insertForumData(postData);
-    res.send(result);
-
-}
+  const postData = req.body;
+  const result = await insertForumData(postData);
+  res.send(result);
+};
 const getForumPost = async (req, res) => {
-    const result = await getForumDataFromCollection(req.params);
-    res.send(result)
-}
+  const result = await getForumDataFromCollection(req.params);
+  res.send(result);
+};
 const getForumPostbymail = async (req, res) => {
-    const result = await getForumDatabymail(req.params);
-    res.send(result)
-}
+  const result = await getForumDatabymail(req.params);
+  res.send(result);
+};
 const postComment = async (req, res) => {
-    const result = await addedCommnetById(req);
-    res.send(result)
-}
+  const result = await addedCommnetById(req);
+  res.send(result);
+};
 
-module.exports = { savedFrormPost, getForumPost, getForumPostbymail, postComment }
+module.exports = {
+  savedFrormPost,
+  getForumPost,
+  getForumPostbymail,
+  postComment,
+};
